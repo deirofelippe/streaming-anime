bc4fed4c10163053efab2864951f5963b984b142
felippedesouza <sergiofelippe.deiro@gmail.com>
2019-12-04 07:13:22 -0300

    inicia o projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav id="nav">
+            <ul id="nav-left">
+                <li><a href="#">ANIMES</a></li>
+                <li><a href="#">MANGÁS</a></li>
+                <li><a href="#">FÓRUM</a></li>
+                <li><a href="#">NOTÍCIAS</a></li>
+                <li><a href="#">GUILDA</a></li>
+            </ul>
+            
+            <ul id="nav-right">
+                <li><a href="#">ALEATÓRIO</a></li>
+                <li><a href="#">LOGIN</a></li>
+                <li>
+                    <div id="search">
+                        <input id="search" type="text" placeholder="Anime, mangá, etc.">
+                        <input id="submit" type="submit" value="">
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section>
+        <main>
+            <div id="featured">
+                <div>
+                    <figure id="anime-cover">
+                        <img src="" alt="Vinland Saga Cover" width="" height="">
+                    </figure>
+                </div>
+                <div>
+                    <figure id="anime-logo">
+                        <img src="" alt="Vinland Saga Logo" width="" height="">
+                    </figure>
+                    <div id="anime-links">
+                        <a href="#">ASSISTIR</a>
+                        <a href="#">MINHA LISTA</a>
+                        <a href="#">MAIS INFORMAÇÕES</a>
+                    </div>
+                    <p>Para vingar a morte de seu pai, Thorfinn terá de cumprir algumas exigências antes de duelar contra Askeladd, assassino de Thors e líder do grupo.</p>
+                </div>
+            </div>
+            
+            <div id="calendar">
+                <div id="days-week">
+                    <button class="" onclick="">DOM</button>
+                    <button class="" onclick="">SEG</button>
+                    <button class="" onclick="">TER</button>
+                    <button class="" onclick="">QUA</button>
+                    <button class="" onclick="">QUI</button>
+                    <button class="" onclick="">SEX</button>
+                    <button class="" onclick="">SAB</button>
+                </div>
+                
+                <div id="anime-day">
+                    <figure id="day-sunday">
+                        <img src="" alt="Imagem 1" width="100" height="300">
+                        <img src="" alt="Imagem 2" width="100" height="300">
+                        <img src="" alt="Imagem 3" width="100" height="300">
+                        <img src="" alt="Imagem 4" width="100" height="300">
+                        <img src="" alt="Imagem 5" width="100" height="300">
+                        <img src="" alt="Imagem 6" width="100" height="300">
+                    </figure>
+                    <figure id="day-monday">
+                        <img src="" alt="Imagem 1" width="100" height="300">
+                        <img src="" alt="Imagem 2" width="100" height="300">
+                        <img src="" alt="Imagem 3" width="100" height="300">
+                        <img src="" alt="Imagem 4" width="100" height="300">
+                        <img src="" alt="Imagem 5" width="100" height="300">
+                        <img src="" alt="Imagem 6" width="100" height="300">
+                    </figure>
+                    <figure id="day-tuesday">
+                        <img src="" alt="Imagem 1" width="100" height="300">
+                        <img src="" alt="Imagem 2" width="100" height="300">
+                        <img src="" alt="Imagem 3" width="100" height="300">
+                        <img src="" alt="Imagem 4" width="100" height="300">
+                        <img src="" alt="Imagem 5" width="100" height="300">
+                        <img src="" alt="Imagem 6" width="100" height="300">
+                    </figure>
+                    <figure id="day-wednesday">
+                        <img src="" alt="Imagem 1" width="100" height="300">
+                        <img src="" alt="Imagem 2" width="100" height="300">
+                        <img src="" alt="Imagem 3" width="100" height="300">
+                        <img src="" alt="Imagem 4" width="100" height="300">
+                        <img src="" alt="Imagem 5" width="100" height="300">
+                        <img src="" alt="Imagem 6" width="100" height="300">
+                    </figure>
+                    <figure id="day-thursday">
+                        <img src="" alt="Imagem 1" width="100" height="300">
+                        <img src="" alt="Imagem 2" width="100" height="300">
+                        <img src="" alt="Imagem 3" width="100" height="300">
+                        <img src="" alt="Imagem 4" width="100" height="300">
+                        <img src="" alt="Imagem 5" width="100" height="300">
+                        <img src="" alt="Imagem 6" width="100" height="300">
+                    </figure>
+                    <figure id="day-friday">
+                        <img src="" alt="Imagem 1" width="100" height="300">
+                        <img src="" alt="Imagem 2" width="100" height="300">
+                        <img src="" alt="Imagem 3" width="100" height="300">
+                        <img src="" alt="Imagem 4" width="100" height="300">
+                        <img src="" alt="Imagem 5" width="100" height="300">
+                        <img src="" alt="Imagem 6" width="100" height="300">
+                    </figure>
+                    <figure id="day-saturday">
+                        <img src="" alt="Imagem 1" width="100" height="300">
+                        <img src="" alt="Imagem 2" width="100" height="300">
+                        <img src="" alt="Imagem 3" width="100" height="300">
+                        <img src="" alt="Imagem 4" width="100" height="300">
+                        <img src="" alt="Imagem 5" width="100" height="300">
+                        <img src="" alt="Imagem 6" width="100" height="300">
+                    </figure>
+                </div>
+            </div>
+            
+            <div id="new-episodes">
+                <header>
+                    <h1><span><strong>ANIMES</strong></span>: NOVOS EPISÓDIOS</h1>
+                </header>
+                
+                <div id="anime-1">
+                    <div>
+                        <p>Episódio 7</p>
+                        <p>há um dia</p>
+                    </div>
+                    <figure><img src="" alt=""></figure>
+                    <div>
+                        <h2>Welcome to Deamon Schoo...</h2>
+                        <p>First Love Memories</p>
+                    </div>
+                </div>
+                <div id="anime-2">
+                    <div>
+                        <p>Episódio 68</p>
+                        <p>há um dia</p>
+                    </div>
+                    <figure><img src="" alt=""></figure>
+                    <div>
+                        <h2>My Hero Academia Season 4</h2>
+                        <p>Lets' Go, Rusty Red Riot</p>
+                    </div>
+                </div>   
+                <div id="anime-3">
+                    <div>
+                        <p>Episódio 20</p>
+                        <p>há um dia</p>
+                    </div>
+                    <figure><img src="" alt=""></figure>
+                    <div>
+                        <h2>Dr. STONE</h2>
+                        <p>The Age of Energy</p>
+                    </div>
+                </div>                    
+                
+                <div>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+            
+            <div id="new-chapters">
+                <header>
+                    <h1><span><strong>MANGÁS</strong></span>: NOVOS CAPÍTULOS</h1>
+                </header>
+                
+                <div>
+                    <div id="manga-1">
+                        <p>Há 7 horas</p>
+                        <figure><img src="" alt=""></figure>
+                        <h2>Dr. STONE</h2>
+                        <p>Capítulo 1</p>
+                    </div>
+                    <div id="manga-2">
+                        <p>Há 7 horas</p>
+                        <figure><img src="" alt=""></figure>
+                        <h2>Demon Slayer</h2>
+                        <p>Capítulo 1</p>
+                    </div>
+                    <div id="manga-3">
+                        <p>Há 7 horas</p>
+                        <figure><img src="" alt=""></figure>
+                        <h2>My Hero Academia</h2>
+                        <p>Capítulo 1</p>
+                    </div>
+                </div>
+                
+                <div>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+            
+            <div id="news">
+                <header>
+                    <h1><span><strong>NOTÍCIAS</strong></span>:</h1>
+                </header>
+                <div>
+                    <div id="news-1">
+                        <figure>
+                            <img src="" alt="">
+                        </figure>
+                        <div>
+                            <h2>Kanna Kii Umibe - Mangá ganha adaptação para anime</h2>
+                            <p>A Fuji TV anunciou que o mangá Kanna Kii Umibe no Étranger (L’étranger du Plage or The Stranger on the Beach) ganhará adaptação em anime no formato filme. Além disso foi revelado que...</p><a href="#">LER MAIS</a>
+                            <p>20/11/2019 - por @IIGon</p>
+                        </div>
+                    </div>
+                </div>
+                <a href="#">MAIS NOTÍCIAS</a>
+            </div>
+            
+            <aside>
+                <div id="popular-animes">
+                    <header>
+                        <h1><span><strong>ANIMES</strong></span>: POPULARES</h1>
+                    </header>
+                    <div>
+                        <div>
+                            <figure><img src="" alt=""></figure>
+                            <h2>BLACK CLOVER</h2>
+                            <p>??? Episódios</p>
+                        </div>
+                        <div>
+                            <figure><img src="" alt=""></figure>
+                            <h2>ASSASSINS PRIDE</h2>
+                            <p>4 Episódios</p>
+                        </div>
+                        <div>
+                            <figure><img src="" alt=""></figure>
+                            <h2>SWORD ART ONLINE</h2>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div id="forum">
+                    <header>
+                        <h1><span><strong>FÓRUM</strong></span></h1>
+                    </header>
+                    <div>
+                        <h2><strong>DISCUSSÃO GERAL</strong></h2>
+                        <div class="cols">
+                            <div class="col-left">
+                                <p>Animes dublados</p>
+                                <p>Carregamento no novo player</p>
+                                <p>Qual é o seu anime favorito</p>
+                                <p>Animes dublados</p>
+                                <p>Carregamento no novo player</p>
+                                <p>Qual é o seu anime favorito</p>
+                            </div>
+                            <div class="col-right">
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                            </div>
+                        </div>
+                        
+                        
+                        <h2><strong>ANIMES</strong></h2>
+                        <div class="cols">
+                            <div class="col-left">
+                                <p>Animes dublados</p>
+                                <p>Carregamento no novo player</p>
+                                <p>Qual é o seu anime favorito</p>
+                                <p>Animes dublados</p>
+                                <p>Carregamento no novo player</p>
+                                <p>Qual é o seu anime favorito</p>
+                            </div>
+                            <div class="col-right">
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                            </div>
+                        </div>
+                        
+                        <h2><strong>ANÚNCIOS</strong></h2>
+                        <div class="cols">
+                            <div class="col-left">
+                                <p>Animes dublados</p>
+                                <p>Carregamento no novo player</p>
+                                <p>Qual é o seu anime favorito</p>
+                                <p>Animes dublados</p>
+                                <p>Carregamento no novo player</p>
+                                <p>Qual é o seu anime favorito</p>
+                            </div>
+                            <div class="col-right">
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                                <p>há 2 horas - "User2314"</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </main>
+    </section>
+    <footer></footer>
+</body>
+</html>
